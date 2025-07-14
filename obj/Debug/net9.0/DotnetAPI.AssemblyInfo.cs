ceeb2ffe8e553acfc6a5cf858676e9da603731a4
RusssianMusketeer <a.devetriev@gmail.com>
2025-07-14 13:32:39 -0400

    login registrations DTO's created

--- obj/Debug/net9.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17df928857011a3cb11c73e53e8f272ea02bc3bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba1d70a592d4c66bb484f04dbf8a857006dcce19")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
