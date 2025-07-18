9c1c32fa1d2e8d0a17c8b3c317b5cef9b0691946
RusssianMusketeer <a.devetriev@gmail.com>
2025-07-17 19:07:09 -0500

    ch

--- obj/Debug/net9.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7184d819c719534084e349125d1ef67c5b14e1a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d9e63f886204c911520b38f5725835c0af0595f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
